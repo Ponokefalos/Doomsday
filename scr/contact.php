5013ca53036235ec7b309ceaf4022c18b0683759
Ponokefalos <pesrekaisi@gmail.com>
2015-06-25 14:49:11 +0300

    contanct form done

--- scr/contact.php
+++ scr/contact.php
@@ -13,6 +13,8 @@
     <link href="../css/navbar.css" rel="stylesheet">
     <link href="../css/globalShadowBoxStyle.css" rel="stylesheet">
 
+    <link href="../css/contanctStyle.css" rel="stylesheet">
+
     <?php include('includes/header.php');
     include('includes/navbar.php');
     ?>
@@ -24,18 +26,61 @@
 <body>
 
 <div class="container marketing">
-    <br>  <br>   <br> <br> <br> <br> <br>
-    <br>  <br>   <br> <br> <br> <br> <br>
-    <br>  <br>   <br> <br> <br> <br> <br>
-    <br>  <br>   <br> <br> <br> <br> <br>
-    <br>  <br>   <br> <br> <br> <br> <br>
-    <br>  <br>   <br> <br> <br> <br> <br>
+    <div class="head_title">
 
+        <p>
+
+        <h3><i>Επικοινωνία</i></h3></p>
+        <hr class="featurette-divider">
+    </div>
+    <!-- =============================================PERIEXOMENO SELIDAS ================================================-->
+
+    <p>Επικοινωνίστε με τους υπεύθυνους της σελίδας, συμπληρώνοντας την παρακάτω φόρμα με τα στοιχεία σας.</p>
+
+
+
+    <form class="form-horizontal" method="post" enctype="multipart/form-data">
+
+        <div class="form-group">
+            <p class="col-xs-2 control-label">E-mail</p>
+
+            <div class="col-sm-4 col-xs-4">
+                <input type="text" class="form-control" name="creator" id="creator"
+                       placeholder="mail επικοινωνίας" required="true">
+            </div>
+        </div>
+        <div class="form-group">
+            <p class="col-xs-2 control-label">Όνομα</p>
+
+            <div class="col-sm-4 col-xs-4">
+                <input type="text" class="form-control" name="creator" id="creator"
+                       placeholder="Όνομα χρήστη" required="true">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <p class="col-xs-2 control-label">Θέμα</p>
+
+            <div class="col-sm-4">
+                        <textarea name="subject" class="form-control user_input" id="subject"
+                                  rows="3"
+                                  placeholder=" Θέμα" required="true"></textarea>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" name="sentMail" class="btn btn-primary">Αποστολή</button>
+            </div>
+        </div>
+    </form>
+    <br> <br> <br> <br> <br> <br> <br>
+    <br> <br> <br> <br> <br> <br> <br>
 
 
 </div>
 
-<?php include('includes/footer.php')?>
+<?php include('includes/footer.php') ?>
 </body>
 
 
